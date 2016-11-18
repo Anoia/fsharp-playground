83680b5b08de819aa83ac443f6cfc7406cf1e1b3
esther.machata <esther.machata@pdfforge.org>
2016-11-18 17:34:23 +0100

    Add FizzBuzz

---
+++ basteltag-fsharp/FizzBuzz.fs
@@ -0,0 +1,21 @@
+﻿namespace MyNamespace
+
+module FizzBuzz =
+    let GetValue x = 
+        match x with
+        | i when i%15 = 0 -> "FizzBuzz"
+        | i when i%3 = 0 -> "Fizz"
+        | i when i%5 = 0 -> "Buzz"
+        | _ -> sprintf "%i" x
+    
+   
+    let DoTheFizzBuzz x = 
+        let folder currentString (number, str) = sprintf "%s %i -> %s\n"  currentString number str
+        
+        let values = [1 .. x]
+
+        List.map GetValue values
+        |> List.zip values
+        |> List.fold folder ""
+    
+
---
+++ basteltag-fsharp/Program.fs
@@ -0,0 +1,20 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+namespace MyNamespace
+
+module Test = 
+    open System
+    open Integers
+    open BasicFunctions
+
+    [<EntryPoint>]
+    let main argv = 
+        //printfn "%A" argv
+
+        Console.ReadLine() 
+            |> Int32.Parse 
+            |> FizzBuzz.DoTheFizzBuzz
+            |> printf "%s"
+
+        Console.ReadLine() |> ignore
+        0 // return an integer exit code 
